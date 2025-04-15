33951e6879482d02c68c43fadb4be7ec18d574eb
marcopra <marco.prattico@live.it>
2025-04-15 16:53:07 +0000

    base_launcher modified

--- base_launcher.sh
+++ base_launcher.sh
@@ -10,6 +10,5 @@ source ~/.bashrc
 conda activate metataco
 
 # python3 train_metaworld.py agent.pretrained_path=none exp_name=taco_with_losses
-# python pretrain_taco.py --dataset_path "data/push-v2_img84_fs3_ar1_exp=60__ds=100000" --use_wandb --total_steps 300_000_000 --checkpoint "50_000_000, 100_000_000"
-python pretrain_taco.py --dataset_path "data/push-v2_img84_fs3_ar1_exp=60__ds=100000" --use_wandb --total_steps 300_000_000 --checkpoint "50_000_000, 100_000_000"
-# python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/models/" exp_name=pretrained_taco
+# python pretrain_taco.py --dataset_path "data/push-v2_img84_fs3_ar1_exp=60__ds=100000" --use_wandb --total_steps 300_000_000 --checkpoint "2_000_000, 20_000_000, 50_000_000, 100_000_000, 200_000_000"
+python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/models/taco_push-v2_img84_fs3_ar1_exp\=60__ds\=100000_ts\=20000768.pt" exp_name=pretrained_taco
