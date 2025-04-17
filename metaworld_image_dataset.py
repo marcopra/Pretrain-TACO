aef9b1e13f17ccd1a6681f24f4db3d193e143f55
marcopra <marco.prattico@live.it>
2025-04-17 10:38:04 +0000

    action repeat

--- metaworld_image_dataset.py
+++ metaworld_image_dataset.py
@@ -76,8 +76,8 @@ def collect_dataset(env_names, expert_probs, dataset_size=int(1e6), checkpoint =
         frame_stack: Number of consecutive frames to stack together
         action_repeat: Number of times to repeat the same action
     """
-    if action_repeat > 1:
-        raise NotImplementedError("Action repeat > 1 is implemented yet, but I m not sure it is necessary ")
+    # if action_repeat > 1:
+    #     raise NotImplementedError("Action repeat > 1 is implemented yet, but I m not sure it is necessary ")
     
     mt10 = metaworld.MT10(seed=42)
     for expert_prob in expert_probs:
@@ -96,7 +96,7 @@ def collect_dataset(env_names, expert_probs, dataset_size=int(1e6), checkpoint =
             task_idx = env_task_indices[task_number]
             # Create environment with image observations using PLEX-MetaWorld
             env = mt10.train_classes[env_name](render_mode="rgb_array", camera_name="corner")
-            env = RandomizeInitialPositionWrapper(env)
+            # env = RandomizeInitialPositionWrapper(env)
             env = ResizeRendering(env, resolution=resolution)
             env.set_task(mt10.train_tasks[task_idx])
             state, info = env.reset()
