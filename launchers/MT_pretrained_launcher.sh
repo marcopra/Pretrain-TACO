82e3e3397340b05be777add8a29c504ca33e7269
marcopra <marco.prattico@live.it>
2025-04-22 13:22:56 +0000

    mt launcher

--- launchers/MT_pretrained_launcher.sh
+++ launchers/MT_pretrained_launcher.sh
@@ -3,10 +3,10 @@
 seeds="0 1 2"
 env_names=("push-v2")
 model_paths=(
-    "models/taco_MT_exp\=80_1OOD_push_config_ts\=200000512.pt"
-    "models/taco_MT_exp\=60_1OOD_push_config_ts\=200000512.pt"
-    "models/taco_MT_exp\=40_1OOD_push_config_ts\=200000512.pt"
-    "models/taco_MT_exp\=20_1OOD_push_config_ts\=200000512.pt"
+    "models/taco_MT_exp=80_1OOD_push_config_ts=200000512.pt"
+    "models/taco_MT_exp=60_1OOD_push_config_ts=200000512.pt"
+    "models/taco_MT_exp=40_1OOD_push_config_ts=200000512.pt"
+    "models/taco_MT_exp=20_1OOD_push_config_ts=200000512.pt"
 )
 
 for seed in $seeds; do
