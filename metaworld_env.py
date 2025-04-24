f78b8c9f734a05ca06d16b9621250524901db3b3
marcopra <marco.prattico@live.it>
2025-04-24 11:04:48 +0000

    typo

--- metaworld_env.py
+++ metaworld_env.py
@@ -244,7 +244,7 @@ class ExtendedTimeStepWrapper(gym.Wrapper):
         return time_step
 
 
-def make(env_name, frame_stack, action_repeat, seed, resolution=84, camera='corner', rondom_init=True):
+def make(env_name, frame_stack, action_repeat, seed, resolution=84, camera='corner', random_init=True):
     """
     Create a MetaWorld environment with image observations, frame stacking, and action repeat.
     
@@ -255,7 +255,7 @@ def make(env_name, frame_stack, action_repeat, seed, resolution=84, camera='corn
         seed: Random seed
         resolution: Image resolution (height and width)
         camera: Camera angle to use
-        rondom_init: Whether to randomize the initial position of the hand and object
+        random_init: Whether to randomize the initial position of the hand and object
     
     Returns:
         A wrapped MetaWorld environment
@@ -274,7 +274,7 @@ def make(env_name, frame_stack, action_repeat, seed, resolution=84, camera='corn
     task_idx = env_task_indices[task_number]
     # Create environment with image observations using PLEX-MetaWorld
     env = mt10.train_classes[env_name](render_mode="rgb_array", camera_name=camera)
-    if rondom_init is True:
+    if random_init is True:
         env = RandomizeInitialPositionWrapper(env)
     env = ResizeRendering(env, resolution=resolution)
     env.set_task(mt10.train_tasks[task_idx])
--- train_metaworld.py
+++ train_metaworld.py
@@ -71,7 +71,7 @@ class Workspace:
             self.cfg.seed,
             resolution=self.cfg.resolution,
             camera=self.cfg.camera,
-            rondom_init=self.cfg.rondom_init,
+            random_init=self.cfg.random_init,
         )
         
         self.eval_env = metaworld_env.make(
@@ -81,7 +81,7 @@ class Workspace:
             self.cfg.seed,
             resolution=self.cfg.resolution,
             camera=self.cfg.camera,
-            rondom_init=self.cfg.rondom_init,
+            random_init=self.cfg.random_init,
         )
         
         # Create replay buffer specs
