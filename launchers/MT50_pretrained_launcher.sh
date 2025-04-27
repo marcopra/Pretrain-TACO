ee4249939d6e819a6a5056cc40b0a95db1070c00
marcopra <marco.prattico@live.it>
2025-04-27 17:10:14 +0000

    launcher update

---
+++ launchers/MT50_pretrained_launcher.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+seeds="0 1 2"
+env_names=("push-v2")
+model_paths=(
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=40_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=80_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=60_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=20_1OOD_push_config_ts=200000512.pt"
+)
+
+for seed in $seeds; do
+    for env_name in "${env_names[@]}"; do
+        for model_path in "${model_paths[@]}"; do
+            echo "Submitting job: ENV_NAME=${env_name}, MODEL_PATH=${model_path}, SEED=${seed}"
+            qsub -v SEED="${seed}",MODEL_PATH="${model_path}",ENV_NAME="${env_name}" launchers/MT_pretrained.sh
+        done
+    done
+done
+
+
