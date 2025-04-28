aeea6f63c08bd24e7e4b3c38c800c02d41c7ad5d
marcopra <marco.prattico@live.it>
2025-04-28 13:09:07 +0000

    update launchers

--- launchers/MT50_pretrained_launcher.sh
+++ launchers/MT50_pretrained_launcher.sh
@@ -3,10 +3,10 @@
 seeds="0 1 2"
 env_names=("push-v2")
 model_paths=(
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=40_1OOD_push_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=80_1OOD_push_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=60_1OOD_push_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=20_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=40_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=80_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=60_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=20_1OOD_push_config_ts=200000512.pt"
 )
 random_hand_initial="false"
 random_goal_n_object_initial="false"
@@ -15,8 +15,10 @@ for random_hand in $random_hand_initial; do
     for random_goal in $random_goal_n_object_initial; do
         for seed in $seeds; do
             for env_name in "${env_names[@]}"; do
+                for model_path in "${model_paths[@]}"; do
                 echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}"
-                qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}" launchers/baseline.sh
+                qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}" launchers/MT_pretrained.sh
+                done
             done
         done
     done
--- launchers/MT_pretrained_launcher.sh
+++ launchers/MT_pretrained_launcher.sh
@@ -3,20 +3,22 @@
 seeds="0 1 2"
 env_names=("push-v2")
 model_paths=(
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_exp=80_1OOD_push_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_exp=60_1OOD_push_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_exp=40_1OOD_push_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_exp=20_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT10/taco_MT_exp=80_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT10/taco_MT_exp=60_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT10/taco_MT_exp=40_1OOD_push_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/MT10/taco_MT_exp=20_1OOD_push_config_ts=200000512.pt"
 )
-random_hand_initial="true false"
-random_goal_n_object_initial="true false"
+random_hand_initial="false"
+random_goal_n_object_initial="false"
 
 for random_hand in $random_hand_initial; do
     for random_goal in $random_goal_n_object_initial; do
-        for env_name in "${env_names[@]}"; do
-            for seed in $seeds; do
+        for seed in $seeds; do
+            for env_name in "${env_names[@]}"; do
+                for model_path in "${model_paths[@]}"; do
                 echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}"
-                qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}" launchers/baseline.sh
+                qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}" launchers/MT_pretrained.sh
+                done
             done
         done
     done
