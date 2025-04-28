8d25148729fc2403d2ef76dca06503b4f2d93437
marcopra <marco.prattico@live.it>
2025-04-28 13:05:42 +0000

    launcher update

--- launchers/MT50_pretrained_launcher.sh
+++ launchers/MT50_pretrained_launcher.sh
@@ -8,13 +8,13 @@ model_paths=(
     "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=60_1OOD_push_config_ts=200000512.pt"
     "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_exp=20_1OOD_push_config_ts=200000512.pt"
 )
-random_hand_initial="true false"
-random_goal_n_object_initial="true false"
+random_hand_initial="false"
+random_goal_n_object_initial="false"
 
 for random_hand in $random_hand_initial; do
     for random_goal in $random_goal_n_object_initial; do
-        for env_name in "${env_names[@]}"; do
-            for seed in $seeds; do
+        for seed in $seeds; do
+            for env_name in "${env_names[@]}"; do
                 echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}"
                 qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}" launchers/baseline.sh
             done
