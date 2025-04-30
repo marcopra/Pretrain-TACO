a15cd98a49bea4fa52c1998dc47833d6742093dd
marcopra <marco.prattico@live.it>
2025-04-30 16:10:10 +0000

    prova

--- launchers/ST50_dataset_collection_launcher.sh
+++ launchers/ST50_dataset_collection_launcher.sh
@@ -42,8 +42,7 @@ for env_list in "${env_lists[@]}"; do
                         
                         # Submit the job with variables
                         qsub -N "$job_name" \
-               
-                            -v ENV_LIST="$env_list",TASK="$task",DATASET_SIZE="$dataset_size",EXPERT_PROBS="$expert_probs",SAVE_PATH="$save_path",RANDOM_INIT="$random_init",RANDOM_GOAL="$random_goal" \
+                            -v ENV_LIST="$env_list",TASKS="$task",DATASET_SIZE="$dataset_size",EXPERT_PROBS="$expert_probs",SAVE_PATH="$save_path",RANDOM_INIT="$random_init",RANDOM_GOAL="$random_goal" \
                             launchers/script_dataset_collection.sh
 
                         echo "Job submitted: $job_name"
--- launchers/ST_dataset_collection_launcher.sh
+++ launchers/ST_dataset_collection_launcher.sh
@@ -36,7 +36,6 @@ for env_list in "${env_lists[@]}"; do
                     
                     # Submit the job with variables
                     qsub -N "$job_name" \
-               
                          -v ENV_LIST="$env_list",DATASET_SIZE="$dataset_size",EXPERT_PROBS="$expert_probs",SAVE_PATH="$save_path",RANDOM_INIT="$random_init",RANDOM_GOAL="$random_goal" \
                          launchers/script_dataset_collection.sh
                     
