52b9856ba717f47010637e934830ac109f770fca
marcopra <marco.prattico@live.it>
2025-05-01 10:16:28 +0000

    launcher

--- launchers/ST50_pretrained_launcher.sh
+++ launchers/ST50_pretrained_launcher.sh
@@ -15,7 +15,7 @@ for random_hand in $random_hand_inital; do
     for seed in $seeds; do
         for env_name in "${env_names[@]}"; do
             for model_path in "${model_paths[@]}"; do
-            echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_hand}, SEED=${seed}"
+            echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=false, SEED=${seed}"
             qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="false",MODEL_PATH="${model_path}",WANDB_TAG="ST50" launchers/MT_pretrained.sh
             done
         done
