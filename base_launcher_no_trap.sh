c4600484c07d989782c8d58d8abec8e7d50bb946
marcopra <marco.prattico@live.it>
2025-05-05 15:55:12 +0000

    updates

--- base_launcher_no_trap.sh
+++ base_launcher_no_trap.sh
@@ -11,8 +11,8 @@ source ~/.bashrc
 conda activate metataco
 
 # python3 train_metaworld.py agent.pretrained_path=none exp_name=taco_with_losses random_init=true device=cuda:0
-python pretrain_taco.py --dataset_path "/fastwork/mprattico/Pretrain-TACO/data/ST/push-v2_img84_fs3_ar2_exp=20__ds=100000" --use_wandb --total_steps 300_000_000 --checkpoint "2_000_000, 20_000_000, 50_000_000, 100_000_000, 200_000_000"
+# python pretrain_taco.py --dataset_path "/fastwork/mprattico/Pretrain-TACO/data/ST/push-v2_img84_fs3_ar2_exp=20__ds=100000" --use_wandb --total_steps 300_000_000 --checkpoint "2_000_000, 20_000_000, 50_000_000, 100_000_000, 200_000_000"
 # python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/models/taco_MT_exp\=80_1OOD_push_config_ts\=200000512.pt" exp_name=pretrained_taco_80200M random_init=true device=cuda:0
-python pretrain_taco_multi_task.py --dataset_config "mt_config/ST/ST_exp=80_push_config.json" --use_wandb --total_steps 300_000_000 --checkpoint "20_000_000,200_000_000" --lr 1e-4 --device "cuda:0"
+python pretrain_taco_multi_task.py --dataset_config "mt_config/ST50/ST50_exp=20_push_fs3_ar2_ri1_rg0_config.json" --use_wandb --total_steps 300_000_000 --checkpoint "20_000_000,200_000_000" --lr 5e-4 
 
 # Cleanup will be triggered automatically by the trap
