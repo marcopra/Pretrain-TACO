417d35a03f3a539a67f3fe21b5c496a419e54f4b
marcopra <marco.prattico@live.it>
2025-05-10 17:03:47 +0000

    launchers

--- launchers/MT50_pretrained_launcher.sh
+++ launchers/MT50_pretrained_launcher.sh
@@ -5,8 +5,8 @@ env_names=("push-v2")
 model_paths=(
     "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.0_lr=0.0005_ts=50000896.pt"
     "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.33_lr=0.0005_ts=50000896.pt"
-    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.66_lr=0.0005_ts=50000896.pt"
-    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.99_lr=0.0005_ts=50000896.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.66_lr=0.0005_ts=50000896.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.99_lr=0.0005_ts=50000896.pt"
     
 )
    
--- launchers/MT50_pretrained_launcher_freeze.sh
+++ launchers/MT50_pretrained_launcher_freeze.sh
@@ -3,10 +3,10 @@
 seeds="1 1 1"
 env_names=("push-v2")
 model_paths=(
-    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=20_1OOD_push_fs3_ar2_ri1_rg0_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=40_1OOD_push_fs3_ar2_ri1_rg0_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=60_1OOD_push_fs3_ar2_ri1_rg0_config_ts=200000512.pt"
-    "/home/mprattico/Pretrain-TACO/models/MT50/taco_MT_MT50_exp=80_1OOD_push_fs3_ar2_ri1_rg0_config_ts=200000512.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.0_lr=0.0005_ts=50000896.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.33_lr=0.0005_ts=50000896.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.66_lr=0.0005_ts=50000896.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_0.99_lr=0.0005_ts=50000896.pt"
 )
    
 random_hand_inital="true"
@@ -20,7 +20,7 @@ for random_hand in $random_hand_inital; do
             for env_name in "${env_names[@]}"; do
                 for model_path in "${model_paths[@]}"; do
                 echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}"
-                qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}",WANDB_TAG="${wandb_tag}",freeze="${FREEZE}" launchers/MT_pretrained.sh
+                qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}",WANDB_TAG="${wandb_tag}",FREEZE="${freeze}" launchers/MT_pretrained.sh
                 done
             done
         done
