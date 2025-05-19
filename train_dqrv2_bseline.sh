c95e6fe1fb7f114372d5e241b46ee516445b6227
marcopra <marco.prattico@live.it>
2025-05-19 13:23:06 +0000

    update

--- train_dqrv2_bseline.sh
+++ train_dqrv2_bseline.sh
@@ -47,3 +47,15 @@ python3 train_metaworld.py --config-name config_drqv2metaworld exp_name=baseline
 
 # Cleanup command
 rm -rf exp_local/metaworld/baseline${cuda_device}${freeze_suffix}*
+
+# Run the Python command with the appropriate arguments
+python3 train_metaworld.py --config-name config_drqv2metaworld exp_name=baseline${cuda_device}${freeze_suffix} seed=1 env_name=push-v2 random_init=true random_goal=false wandb_tag=baseline num_train_frames=300000 device=cuda:${cuda_device} agent.freeze_encoder=${freeze_encoder}
+
+# Cleanup command
+rm -rf exp_local/metaworld/baseline${cuda_device}${freeze_suffix}*
+
+# Run the Python command with the appropriate arguments
+python3 train_metaworld.py --config-name config_drqv2metaworld exp_name=baseline${cuda_device}${freeze_suffix} seed=1 env_name=push-v2 random_init=true random_goal=false wandb_tag=baseline num_train_frames=300000 device=cuda:${cuda_device} agent.freeze_encoder=${freeze_encoder}
+
+# Cleanup command
+rm -rf exp_local/metaworld/baseline${cuda_device}${freeze_suffix}*
