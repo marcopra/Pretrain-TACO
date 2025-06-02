bc7cef325513adbffff39ac2fe33e22ec6fc4313
marcopra <marco.prattico@live.it>
2025-06-02 13:21:23 +0000

    launcher bug

--- launchers/TACO/resnet/RESNET_pretrained_launcher_freeze.sh
+++ launchers/TACO/resnet/RESNET_pretrained_launcher_freeze.sh
@@ -11,10 +11,8 @@ for random_hand in $random_hand_inital; do
     for random_goal in $random_goal_inital; do
         for seed in $seeds; do
             for env_name in "${env_names[@]}"; do
-                for model_path in "${model_paths[@]}"; do
                 echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}"
                 qsub -v SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",WANDB_TAG="${wandb_tag}",FREEZE="${freeze}" launchers/TACO/resnet/RESNET_imagenet_pretrained.sh
-                done
             done
         done
     done
