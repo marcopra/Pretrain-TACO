c0dbfb4b774bbfb606ef46d9b14e3c2f7ece116e
marcopra <marco.prattico@live.it>
2025-06-06 19:50:12 +0000

    launcher

--- base_launcher_no_trap.sh
+++ base_launcher_no_trap.sh
@@ -2,6 +2,7 @@
 #PBS -l select=1:ncpus=4:ngpus=1
 #PBS -l walltime=24:00:00
 #PBS -j oe
+#PBS -q a100f
 
 cd $PBS_O_WORKDIR
 
@@ -10,9 +11,5 @@ cd $PBS_O_WORKDIR
 source ~/.bashrc
 conda activate metataco
 
-# python3 train_metaworld.py agent.pretrained_path=none exp_name=taco_with_losses random_init=true device=cuda:0
-# python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/models/taco_MT_exp\=80_1OOD_push_config_ts\=200000512.pt" exp_name=pretrained_taco_80200M random_init=true device=cuda:0
-# python pretrain_taco_multi_task_episodes.py --dataset_config "data_episodes1/ST50" --use_wandb --total_steps 300_000_000 --checkpoint "20_000_000,100_000_000,200_000_000" --lr 5e-4
-python pretrain_taco_multi_task_episodes.py --dataset_config "data_episodes/ST50/0.66" --use_wandb --total_steps 300_000_000 --checkpoint "100_000, 50_000_000, 100_000_000, 200_000_000" --lr 5e-4 --save_path "models/debug/"
-
-# Cleanup will be triggered automatically by the trap
+python train_metaworld.py agent=taco_resnet agent.pretrained_path="resnet50_l5" exp_name="RESNET_longexpresnet50_l5_BS240_820_random_init_true_random_goal_false_freeze_false" seed=820 env_name=push-v2 random_init=true random_goal=false wandb_tag=RESNET50-FREEZE num_train_frames=300000 agent.freeze_encoder=false batch_size=240 save_snapshot=true wandb_id=7sycw7fs
+python train_metaworld.py agent=taco_resnet agent.pretrained_path="resnet50_l5" exp_name="RESNET_longexpresnet50_l5_BS240_1570_random_init_true_random_goal_false_freeze_false" seed=1570 env_name=push-v2 random_init=true random_goal=false wandb_tag=RESNET50-FREEZE num_train_frames=300000 agent.freeze_encoder=false batch_size=240 save_snapshot=true wandb_id=37fzd208
