596fee2671000df9f0881a32bc431c559c49460e
marcopra <marco.prattico@live.it>
2025-06-12 12:03:26 +0000

    slurm launcher

---
+++ slurm_launcher_multigpu.sh
@@ -0,0 +1,20 @@
+    #!/bin/bash
+#SBATCH --job-name=metataco_train
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH --output=job_%j.out          
+#SBATCH --error=%j.err
+
+cd $SLURM_SUBMIT_DIR
+
+# Load environment
+source ~/.bashrc
+module unload anaconda3/2023.09-0
+module load anaconda3/2023.09-0
+conda activate metataco
+module unload anaconda3/2023.09-0
+
+torchrun --nproc-per-node=4 train_metaworld_ed4ct.py batch_size=256 env_name=push-v3
