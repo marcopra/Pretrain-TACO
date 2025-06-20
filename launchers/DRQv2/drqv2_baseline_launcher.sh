8c870c39e45df1cd1479bb7cfbcc603a861be502
marcopra <marco.prattico@live.it>
2025-06-20 20:43:21 +0000

    launcher update

--- launchers/DRQv2/drqv2_baseline_launcher.sh
+++ launchers/DRQv2/drqv2_baseline_launcher.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 seeds="1 1 1 1 1 1 1 1 1 1 1"
-env_names=("push-v2")
+env_names=("basketball-v2" "button-press-v2" "bin-picking-v2" "shelf-place-v2")
 random_hand_initial="true"
 random_goal_n_object_initial="false"
 # Loop through each environment name and seed   
 
-for random_hand in $random_hand_initial; do
-    for random_goal in $random_goal_n_object_initial; do
-        for env_name in "${env_names[@]}"; do
-            for seed in $seeds; do
+for seed in $seeds; do
+    for random_hand in $random_hand_initial; do
+        for random_goal in $random_goal_n_object_initial; do
+            for env_name in "${env_names[@]}"; do
                 qsub -v SEED="$seed",ENV_NAME="$env_name",RANDOM_HAND="$random_hand",RANDOM_GOAL="$random_goal" launchers/DRQv2/drqv2_baseline.sh
             done
         done
