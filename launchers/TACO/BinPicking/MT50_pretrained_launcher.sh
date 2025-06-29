653497643a05041446b9662a6a59519e9f38df84
marcopra <marco.prattico@live.it>
2025-06-29 15:05:58 +0000

    mt20 models

--- launchers/TACO/BinPicking/MT50_pretrained_launcher.sh
+++ launchers/TACO/BinPicking/MT50_pretrained_launcher.sh
@@ -7,11 +7,12 @@ model_paths=(
     # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_OOD_BinPicking_0.33_lr=0.0005_ts=200000512_curl_rew.pt"
     # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_OOD_BinPicking_0.66_lr=0.0005_ts=200000512_curl_rew.pt"
     # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT50_OOD_BinPicking_0.99_lr=0.0005_ts=50000896_curl_rew.pt" 
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT1_49OOD_ID_DrawerOpen_0.0_lr=0.0005_ts=25344000_curl_rew_best.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT5_45OOD_ID_DisassembleDrawerOpenLeverPullPlateSlideSweep_0.0_lr=0.0005_ts=1894400_curl_rew_best.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT10_40OOD_ID_ButtonPressTopdownWallDisassembleDoorOpenDrawerOpenHandlePressLeverPullPlateSlideBackSidePlateSlideStickPullSweep_0.0_lr=0.0005_ts=69171200_curl_rew_best.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT30_20OOD_AssemblyBasketballBinPickingButtonPressCoffeePullCPushDialTurnFaucetCloseHammerHandlePressSidePickOutHolePPlacePPWallPushPWallReachRWallShelfPlaceWindowCloseWOpen_0.0_lr=0.0005_ts=149043200_curl_rew_best.pt"
-    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT40_10OOD_BasketballBinPickingButtonPressCoffeePushHammerPickPlacePushReachShelfPlaceWindowClose_0.0_lr=0.0005_ts=155699200_curl_rew_best.pt"
+    "/home/mprattico/Pretrain-TACO/models/taco_MT_MT20_30OOD_ID_ButtonPressTopdownBPTWallCoffeeButtonDisassembleDoorLockDOpenDrawerCloseDOpenHandInsertHPressHPullLeverPPegUnplugSidePlateSSidePlateSSoccerStickPullSweepIntoS_0.0_lr=0.0005_ts=77977600_curl_rew_best.pt"
+    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT1_49OOD_ID_DrawerOpen_0.0_lr=0.0005_ts=25344000_curl_rew_best.pt"
+    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT5_45OOD_ID_DisassembleDrawerOpenLeverPullPlateSlideSweep_0.0_lr=0.0005_ts=1894400_curl_rew_best.pt"
+    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT10_40OOD_ID_ButtonPressTopdownWallDisassembleDoorOpenDrawerOpenHandlePressLeverPullPlateSlideBackSidePlateSlideStickPullSweep_0.0_lr=0.0005_ts=69171200_curl_rew_best.pt"
+    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT30_20OOD_AssemblyBasketballBinPickingButtonPressCoffeePullCPushDialTurnFaucetCloseHammerHandlePressSidePickOutHolePPlacePPWallPushPWallReachRWallShelfPlaceWindowCloseWOpen_0.0_lr=0.0005_ts=149043200_curl_rew_best.pt"
+    # "/home/mprattico/Pretrain-TACO/models/taco_MT_MT40_10OOD_BasketballBinPickingButtonPressCoffeePushHammerPickPlacePushReachShelfPlaceWindowClose_0.0_lr=0.0005_ts=155699200_curl_rew_best.pt"
 
 )
    
