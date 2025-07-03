a35b885008de37ca1cd948c869c3044469ca86bd
marcopra <marco.prattico@live.it>
2025-07-03 11:47:10 +0000

    small bugs

--- launchers/pretraining/pretraining.sh
+++ launchers/pretraining/pretraining.sh
@@ -14,4 +14,4 @@ source ~/.bashrc
 conda activate metataco
 
 echo "Running pretraining with dataset: ${DATASET_NAME}, exps: ${EXPS}"
-python pretrain_taco_multi_task_episodes_from_checkpoint.py --dataset_config "data_episodes/${DATASET_NAME}/${EXPS}" --use_wandb --total_steps 200_000_000 --checkpoint "100_000_000, 200_000_000" --lr 5e-4 --save_path models_exp99 --homogeneous. --max_size 98000
+python pretrain_taco_multi_task_episodes_from_checkpoint_homogeneous.py --dataset_config "data_episodes/${DATASET_NAME}/${EXPS}" --use_wandb --total_steps 200_000_000 --checkpoint "100_000_000, 200_000_000" --lr 5e-4 --save_path models_exp99 --homogeneous. --max_size 98000
--- launchers/pretraining/pretraining_launcher_exp.sh
+++ launchers/pretraining/pretraining_launcher_exp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define parameter combinations
-dataset_names=("MT50_OOD_Basketball" "MT50_OOD_BinPicking" "MT50_OOD_ButtonPress" "MT50_OOD_ShelfPlace")
+dataset_names=("MT50_OOD_Push" "MT50_OOD_Basketball" "MT50_OOD_BinPicking" "MT50_OOD_ButtonPress" "MT50_OOD_ShelfPlace")
 exps_values=("0.99")
 
 # Submit jobs for all combinations
