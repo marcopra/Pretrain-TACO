2c0b46845e99d0daf6ebbb483576003e6ff75312
marcopra <marco.prattico@live.it>
2025-07-08 13:21:06 +0000

    wandb_project changed

--- minicluster/ShelfPlace/train_taco_MT1_shelf_place.sh
+++ minicluster/ShelfPlace/train_taco_MT1_shelf_place.sh
@@ -62,7 +62,7 @@ for i in $(seq 1 $num_runs); do
     echo "Running experiment $i of $num_runs"
     
     # Run the Python command with the appropriate arguments
-    python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/${model_path}"  exp_name="/home/mprattico/Pretrain-TACO/${model_path}" seed=1 env_name=shelf-place-v2 random_init=true random_goal=false wandb_tag=MT1${wandb_suffix} num_train_frames=300000 device=cuda:${cuda_device} agent.no_taco=${no_taco}
+    python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/${model_path}"  exp_name="/home/mprattico/Pretrain-TACO/${model_path}" seed=1 env_name=shelf-place-v2 random_init=true random_goal=false wandb_tag=MT1${wandb_suffix} wandb_project="taco_MT" num_train_frames=300000 device=cuda:${cuda_device} agent.no_taco=${no_taco}
 
     # Cleanup command
     rm -rf exp_local/metaworld/home/mprattico/Pretrain-TACO/${model_path}
