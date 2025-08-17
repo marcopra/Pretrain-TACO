9a8d2c8904303018c52fc05e260ff4cfd8adb2f7
marcopra <marco.prattico@live.it>
2025-08-17 23:48:08 +0000

    launchers update

--- launchers/SLURM/BASELINE/frozen_all_baseline.sh
+++ launchers/SLURM/BASELINE/frozen_all_baseline.sh
@@ -19,15 +19,18 @@ random_goal_inital="false"
 no_taco="false"
 freeze="true"
 
+freeze="false"
+agent="taco_resnet"
 for random_hand in $random_hand_inital; do
     for random_goal in $random_goal_inital; do
         for seed in $seeds; do
             for env_name in "${env_names[@]}"; do
                 for model_path in "${model_paths[@]}"; do
                 echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}"
-                sbatch --export=SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}",FREEZE="${freeze}",NO_TACO="${no_taco}",WANDB_PROJECT="taco_frozen_baselines" launchers/SLURM/BASELINE/baseline_pretrained.sh
+                sbatch --export=SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}",FREEZE="${freeze}",NO_TACO="${no_taco}",agent="${}",WANDB_PROJECT="taco_frozen_baselines" launchers/SLURM/BASELINE/baseline_pretrained.sh
                 done
             done
         done
     done
 done
+
