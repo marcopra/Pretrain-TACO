aa01960e919f8fe0a465b5b54e684031d127bff1
marcopra <marco.prattico@live.it>
2025-08-28 15:22:54 +0000

    miniclaster launcher

--- minicluster/Basketball/train_taco_exp_multihead_basketball_long.sh
+++ minicluster/Basketball/train_taco_exp_multihead_basketball_long.sh
@@ -74,7 +74,7 @@ for i in $(seq 1 $num_runs); do
     echo "Running experiment $i of $num_runs"
     
     # Run the Python command with the appropriate arguments
-    python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/${model_path}" exp_name="/home/mprattico/Pretrain-TACO/${model_path}" seed=1 env_name=basketball-v2 random_init=true random_goal=false wandb_tag=${wandb_tag} wandb_project="taco_multihead_long" num_train_frames=2100000 device=cuda:${cuda_device} agent.freeze_encoder=${freeze_encoder}
+    python3 train_metaworld.py agent.pretrained_path="/home/mprattico/Pretrain-TACO/${model_path}" exp_name="/home/mprattico/Pretrain-TACO/${model_path}" seed=1 env_name=basketball-v2 random_init=true random_goal=false wandb_tag=${wandb_tag} wandb_project="taco_multihead_long" num_train_frames=1100000 device=cuda:${cuda_device} agent.freeze_encoder=${freeze_encoder}
 
     # Cleanup command
     rm -rf exp_local/metaworld/home/mprattico/Pretrain-TACO/${model_path}
