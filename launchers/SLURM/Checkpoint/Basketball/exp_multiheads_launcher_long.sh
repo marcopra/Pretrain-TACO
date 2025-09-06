209cd185af2fdaf907b02b5a5a792c4f0a7ca834
marcopra <marco.prattico@live.it>
2025-09-06 10:46:41 +0000

    launchers

---
+++ launchers/SLURM/Checkpoint/Basketball/exp_multiheads_launcher_long.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+seeds="1"
+env_names=("basketball-v2")
+model_paths=(
+"/home/mprattico/Pretrain-TACO/models_checkpoint/Basketball/encoder_basketball-v2_0_0pct_rew0_step0_ep0.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/Basketball/encoder_basketball-v2_0_20pct_rew2237_step30000_ep120.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/Basketball/encoder_basketball-v2_0_40pct_rew2237_step30000_ep120.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/Basketball/encoder_basketball-v2_0_60pct_rew2864_step50000_ep200.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/Basketball/encoder_basketball-v2_0_100pct_rew4284_step120000_ep480.pt"
+)
+   
+random_hand_inital="true"
+random_goal_inital="false"
+wandb_tags=("0_BASKETBALL" "20_BASKETBALL" "40_BASKETBALL" "60_BASKETBALL" "100_BASKETBALL")
+no_taco="false"
+num_steps=2100000
+
+for random_hand in $random_hand_inital; do
+    for random_goal in $random_goal_inital; do
+        for seed in $seeds; do
+            for env_name in "${env_names[@]}"; do
+                for i in "${!model_paths[@]}"; do
+                    model_path="${model_paths[$i]}"
+                    wandb_tag="${wandb_tags[$i]}"
+                    echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}, TAG=${wandb_tag}"
+                    sbatch --export=SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}",WANDB_TAG="${wandb_tag}",NO_TACO="${no_taco}",WANDB_PROJECT="checkpoint",NUM_STEPS="${num_steps}" launchers/SLURM/Checkpoint/Basketball/MT_pretrained.sh
+                done
+            done
+        done
+    done
+done
---
+++ launchers/SLURM/Checkpoint/ButtonPress/exp_multiheads_launcher_long.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+seeds="1"
+env_names=("button-press-v2")
+model_paths=(
+"/home/mprattico/Pretrain-TACO/models_checkpoint/ButtonPress/encoder_button-press-v2_0_0pct_rew0_step0_ep0.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/ButtonPress/encoder_button-press-v2_0_20pct_rew890_step195000_ep780.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/ButtonPress/encoder_button-press-v2_0_40pct_rew3632_step220000_ep880.pt"
+"/home/mprattico/Pretrain-TACO/models_checkpoint/ButtonPress/encoder_button-press-v2_0_60pct_rew3632_step220000_ep880.pt"
+)
+   
+random_hand_inital="true"
+random_goal_inital="false"
+wandb_tags=("0_BUTTON_PRESS" "20_BUTTON_PRESS" "40_BUTTON_PRESS" "60_BUTTON_PRESS")
+no_taco="false"
+num_steps=2100000
+
+for random_hand in $random_hand_inital; do
+    for random_goal in $random_goal_inital; do
+        for seed in $seeds; do
+            for env_name in "${env_names[@]}"; do
+                for i in "${!model_paths[@]}"; do
+                    model_path="${model_paths[$i]}"
+                    wandb_tag="${wandb_tags[$i]}"
+                    echo "Submitting job: ENV_NAME=${env_name}, RANDOM_HAND=${random_hand}, RANDOM_GOAL=${random_goal}, SEED=${seed}, TAG=${wandb_tag}"
+                    sbatch --export=SEED="${seed}",ENV_NAME="${env_name}",RANDOM_HAND="${random_hand}",RANDOM_GOAL="${random_goal}",MODEL_PATH="${model_path}",WANDB_TAG="${wandb_tag}",NO_TACO="${no_taco}",WANDB_PROJECT="checkpoint",NUM_STEPS="${num_steps}" launchers/SLURM/Checkpoint/ButtonPress/MT_pretrained.sh
+                done
+            done
+        done
+    done
+done
