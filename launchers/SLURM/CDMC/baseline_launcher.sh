d15d830469069ceb93ccbd8ef4330c3dc1b7b254
marcopra <marco.prattico@live.it>
2025-09-22 14:06:35 +0000

    launchers

--- launchers/SLURM/CDMC/baseline_launcher.sh
+++ launchers/SLURM/CDMC/baseline_launcher.sh
@@ -8,7 +8,7 @@ wandb_tag="BASELINE"
 for seed in $seeds; do
     for env_name in "${task_names[@]}"; do
         echo "Submitting job: ENV_NAME=${env_name}, SEED=${seed}"
-        sbatch --export=SEED="${seed}",TASK="${env_name}",WANDB_TAG="${wandb_tag}",NO_TACO="${no_taco}",WANDB_PROJECT="taco_cdmc" ../MT_pretrained.sh
+        sbatch --export=SEED="${seed}",TASK="${env_name}",WANDB_TAG="${wandb_tag}",NO_TACO="${no_taco}",WANDB_PROJECT="taco_cdmc" launchers/SLURM/CDMC/MT_pretrained.sh
     done
 done
 
