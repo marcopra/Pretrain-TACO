5e4ce7d49b6c1616ab70dbf6eecd22a973787654
marcopra <marco.prattico@live.it>
2025-09-30 13:50:24 +0000

    launcher

--- base_launcher_slurm.sh
+++ base_launcher_slurm.sh
@@ -14,4 +14,6 @@ cd $SLURM_SUBMIT_DIR
 source ~/.bashrc
 conda activate metataco
 
-python pretrain_ST.py --dataset_config /home/mprattico/Pretrain-TACO/dataset/random_medium --save_path models/maze/random_resnet18 -fe resnet18p --total_steps 300_000_000 --checkpoint "250_000_000,300_000_000" --lr 5e-4 --use_wandb --height 224 --width 224
+# python pretrain_ST.py --dataset_config /home/mprattico/Pretrain-TACO/dataset/random_medium --save_path models/maze/random_resnet18 -fe resnet18 --total_steps 300_000_000 --checkpoint 250_000_000,300_000_000 --lr 5e-4 --use_wandb --resume_wandb_run hdee1avj --resume_checkpoint /home/mprattico/Pretrain-TACO/models/maze/random_resnet18/taco_ST_resnet18_home_mprattico_Pretrain-TACO_dataset_random_medium_l\=0.0005_ts\=63283200_curl_rew_best.pt
+# python train_gym_checkpoint.py agent=taco_checkpoint agent._target__=agents.taco_resnet.TACOAgent save_snapshot=false seed=0 freeze_encoder=true agent.pretrained_path=/home/mprattico/Pretrain-TACO/models/maze/random_resnet18/taco_ST_resnet18_home_mprattico_Pretrain-TACO_dataset_random_medium_l\=0.0005_ts\=63283200_curl_rew_best.pt exp_name=random_resnet_frozen
+python train_gym_checkpoint.py agent=taco_checkpoint agent._target__=agents.taco_resnet.TACOAgent save_snapshot=false seed=0 freeze_encoder=False agent.pretrained_path=/home/mprattico/Pretrain-TACO/models/maze/random_resnet18/taco_ST_resnet18_home_mprattico_Pretrain-TACO_dataset_random_medium_l\=0.0005_ts\=63283200_curl_rew_best.pt exp_name=random_resnet
